f9219f4eb2789215685c2ce4a8438c4a448953b0
Lilian Billot <lilianbillot.metz@gmail.com>
2019-12-05 13:54:16 +0100

    Ajout titre onglet + logo

--- Inscription.php
+++ Inscription.php
@@ -1,1 +1,9 @@
-<body><p>TEST</p></body>
+<head>
+    <title>
+        INSCRIPTION
+    </title>
+    <link rel="icon" href="img/LogoInscription.ico" />
+</head>
+    <body>
+        <p>TEST</p>
+    </body>
